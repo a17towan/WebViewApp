7cdb3c419de406034f1250f0355aa82732eecbef
a17towan <a17towan@student.his.se>
2019-04-04 15:46:17 +0200

    Located my webview element by ID and made url to my Cozmo app

--- app/src/main/java/com/example/brom/webviewapp/MainActivity.java
+++ app/src/main/java/com/example/brom/webviewapp/MainActivity.java
@@ -23,19 +23,10 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        WebView wv = (WebView) findViewById(R.id.my_webview);
-        setContentView(wv);
-        wv.loadUrl("https://scio.his.se");
+        WebView webView =  new WebView (this);
+        setContentView(webView);
+        webView.loadUrl("https://wwwlab.iit.his.se/a17towan/Mobilapplikationsdesign/Projekt/projekt.html");
 
-        // The FAB-code can be removed
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Towe is a star", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
         // 1. Create a WebView element in the layout file content_main.xml
         // -- Commit and push to your github fork
         // 2. Give the WebView element created in step 1 ID "my_webview"
